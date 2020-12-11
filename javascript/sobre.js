7805520a2136d843d90518307e1a4f5c9da7c800
Cainã Moara <moaraadrean@gmail.com>
2020-12-11 17:12:29 -0300

    Alterando funcionamento da aba sobre

--- javascript/sobre.js
+++ javascript/sobre.js
@@ -37,40 +37,64 @@ function toggle() {
 
 function openMore () {
    const open = document.getElementById('more')
-   
+   const close = document.getElementById('more1')
+   const close1 = document.getElementById('more2')
+   const close2 = document.getElementById('more3')
+
    if (open.style.display === 'block') {
     open.style.display = 'none'
     } else {
     open.style.display = 'block'
+    close.style.display = 'none'
+    close1.style.display = 'none'
+    close2.style.display = 'none'
     }
 }
 
 function openMore1 () {
     const open = document.getElementById('more1')
+    const close = document.getElementById('more')
+    const close1 = document.getElementById('more2')
+    const close2 = document.getElementById('more3')
     
     if (open.style.display === 'block') {
         open.style.display = 'none'
     } else {
         open.style.display = 'block'
+        close.style.display = 'none'
+        close1.style.display = 'none'
+        close2.style.display = 'none'
     }
  }
 
 function openMore2 () {
     const open = document.getElementById('more2')
+    const close = document.getElementById('more')
+    const close1 = document.getElementById('more1')
+    const close2 = document.getElementById('more3')
     
     if (open.style.display === 'block') {
         open.style.display = 'none'
     } else {
         open.style.display = 'block'
+        close.style.display = 'none'
+        close1.style.display = 'none'
+        close2.style.display = 'none'
     }
 }
 
 function openMore3 () {
     const open = document.getElementById('more3')
+    const close = document.getElementById('more')
+    const close1 = document.getElementById('more1')
+    const close2 = document.getElementById('more2')
     
     if (open.style.display === 'block') {
         open.style.display = 'none'
     } else {
         open.style.display = 'block'
+        close.style.display = 'none'
+        close1.style.display = 'none'
+        close2.style.display = 'none'
     }
 }
