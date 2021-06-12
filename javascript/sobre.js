dd322d56b73b5d1138e774afe8c9b44f1fe5d4ea
Cainã <moaraadrean@gmail.com>
2021-06-11 23:09:51 -0300

    Refatorando sobre.js

--- javascript/sobre.js
+++ javascript/sobre.js
@@ -5,96 +5,26 @@ function toggle() {
     nav.classList.toggle('active')
 }
 
-// function mouseOver() {
-//     const over = document.getElementById('more')
-//     over.style.display = ("block")
-// }
-
-// function mouseOut() {
-//     const out = document.getElementById('more')
-//     out.style.display = ('none')
-// }
-
-// function mouseOver1() {
-//     const over = document.getElementById('more1')
-//     over.style.display = ("block")
-// }
-
-// function mouseOut1() {
-//     const out = document.getElementById('more1')
-//     out.style.display = ('none')
-// }
-
-// function mouseOver2() {
-//     const over = document.getElementById('more2')
-//     over.style.display = ("block")
-// }
-
-// function mouseOut2() {
-//     const out = document.getElementById('more2')
-//     out.style.display = ('none')
-// }
-
-function openMore () {
-   const open = document.getElementById('more')
-   const close = document.getElementById('more1')
-   const close1 = document.getElementById('more2')
-   const close2 = document.getElementById('more3')
-
-   if (open.style.display === 'block') {
-    open.style.display = 'none'
-    } else {
-    open.style.display = 'block'
-    close.style.display = 'none'
-    close1.style.display = 'none'
-    close2.style.display = 'none'
-    }
+function openMore(id) {
+    Array.from(document.querySelectorAll(`[id^='more']`))
+        .map(e => {
+            if (e.id === id) e.style.display = 'block';
+            else e.style.display = 'none';
+        })
+   
 }
-
-function openMore1 () {
-    const open = document.getElementById('more1')
-    const close = document.getElementById('more')
-    const close1 = document.getElementById('more2')
-    const close2 = document.getElementById('more3')
+// function openMoreAntiga () {
+//     const open = document.getElementById('more1')
+//     const close = document.getElementById('more')
+//     const close1 = document.getElementById('more2')
+//     const close2 = document.getElementById('more3')
     
-    if (open.style.display === 'block') {
-        open.style.display = 'none'
-    } else {
-        open.style.display = 'block'
-        close.style.display = 'none'
-        close1.style.display = 'none'
-        close2.style.display = 'none'
-    }
- }
-
-function openMore2 () {
-    const open = document.getElementById('more2')
-    const close = document.getElementById('more')
-    const close1 = document.getElementById('more1')
-    const close2 = document.getElementById('more3')
-    
-    if (open.style.display === 'block') {
-        open.style.display = 'none'
-    } else {
-        open.style.display = 'block'
-        close.style.display = 'none'
-        close1.style.display = 'none'
-        close2.style.display = 'none'
-    }
-}
-
-function openMore3 () {
-    const open = document.getElementById('more3')
-    const close = document.getElementById('more')
-    const close1 = document.getElementById('more1')
-    const close2 = document.getElementById('more2')
-    
-    if (open.style.display === 'block') {
-        open.style.display = 'none'
-    } else {
-        open.style.display = 'block'
-        close.style.display = 'none'
-        close1.style.display = 'none'
-        close2.style.display = 'none'
-    }
-}
+//     if (open.style.display === 'block') {
+//         open.style.display = 'none'
+//     } else {
+//         open.style.display = 'block'
+//         close.style.display = 'none'
+//         close1.style.display = 'none'
+//         close2.style.display = 'none'
+//     }
+//  }
